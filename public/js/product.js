50239383e90291bf8ca6e3120221a1fe39ea2a73
paulocabrera90 <paulocabrera90@gmail.com>
2024-04-23 04:36:38 -0300

    eventos de botones al agregar productos

--- public/js/product.js
+++ public/js/product.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const btnAgregarAlCarrito = document.querySelectorAll('.add-to-cart-button');
-    const spinnerQuantity = document.querySelectorAll('.product-quantity-spinner');
-    const btnCarrito = document.querySelectorAll('.cart-button');
+    const spinnerQuantity = document.querySelectorAll('.product-quantity-spinner-counter');
+    const btnCartCount = document.querySelector('.count-circle');
+
+    let cantidadProductos;
+    countBtnCart(btnCartCount);
+
+    spinnerQuantity.forEach(spinner => {
+        spinner.addEventListener('change', () => {
+            console.log("Valor spinner", parseInt(spinner.value));
+            cantidadProductos = parseInt(spinner.value);
+            if (cantidadProductos > 10) {
+                alert("La cantidad máxima de un mismo producto es 10");
+                cantidadProductos = spinner.value = 1;
+            }
+        });
+    });
 
     btnAgregarAlCarrito.forEach(btn => {
         console.log("Iniciando accion del boton añadir carrito");        
@@ -15,7 +29,10 @@ document.addEventListener('DOMContentLoaded', () => {
             const productTitle = dataSetProduct.title;
             const productPrice = dataSetProduct.price;
             const productDescription = dataSetProduct.description;
+            const productAmountOff = dataSetProduct.amountOff;
+            const productAmountFinal = dataSetProduct.amountFinal;
             const productImg = dataSetProduct.image;
+            const productOffer = dataSetProduct.offer;
 
             const product = {
                 productId,
@@ -23,19 +40,35 @@ document.addEventListener('DOMContentLoaded', () => {
                 productPrice,
                 productImg,
                 productDescription,
-                productQuantity: 1
+                productAmountOff,
+                productAmountFinal,
+                productOffer,
+                productQuantity: cantidadProductos || 1
             };
 
             console.log("Product add car", product);
 
             let cartAdd = JSON.parse(localStorage.getItem('productsForCart')) || [];
-            console.log("LocalStorage", cartAdd);
 
-            cartAdd.push(product);
-            console.log("cartAdd", cartAdd);         
+            const existProduct = cartAdd.findIndex(item => item.productId === productId);
+            if (existProduct !== -1) {
+                cartAdd[existProduct].productQuantity += cantidadProductos;
+            } else {
+                cartAdd.push(product);
+            } 
 
             localStorage.setItem('productsForCart', JSON.stringify(cartAdd));
+            countBtnCart(btnCartCount);
         });
 
     });
 });
+
+function countBtnCart(btnCartCount) {
+    let cartAdd = JSON.parse(localStorage.getItem('productsForCart')) || [];
+
+    const totalQuantity = cartAdd.reduce((acc, curr) => acc + curr.productQuantity, 0);
+    console.log("Cantidad para el carrito", totalQuantity);
+    btnCartCount.textContent = totalQuantity;    
+
+}
--- src/utils/productsUtils.js
+++ src/utils/productsUtils.js
@@ -1,14 +1,13 @@
-const { json } = require("express");
-
 const porcentajes = [10, 15, 20];
 
 function addOfferFields(products) {
     return products.map(product => {
         const newProduct = { ...product };
         newProduct.offer = Math.random() < 0.5;
-        if (newProduct.offer) {
-          
+        if (newProduct.offer) {          
             newProduct.offerPercentage = porcentajes[Math.floor(Math.random() * 3)];
+            newProduct.amountOff = product.price * (newProduct.offerPercentage / 100);
+            newProduct.amountFinal = product.price - newProduct.amountOff;
         } else {
             delete newProduct.offerPercentage;
         }
